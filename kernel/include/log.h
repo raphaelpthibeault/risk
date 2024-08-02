8c06d9121c9a37e88c825a32c19444cf8e5a1482
rpthibeault <rpthibeault@outlook.com>
2024-08-02 13:13:49 -0400

    organization

---
+++ kernel/include/log.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stdarg.h>
+#include <printf.h>
+#include <serial.h>
+
+void log_(const char *, ...);
+
---
+++ kernel/include/printf.h
@@ -0,0 +1,9 @@
+#pragma once 
+
+#include <stdarg.h>
+#include <stdint.h>
+#include <stddef.h>
+
+char *itoa(uint64_t, char *, int);
+int vsnprintf(char *, size_t, const char *, va_list);
+
