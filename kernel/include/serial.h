9953457a276a75323ca7fdb3220e7b41e7d9e637
rpthibeault <rpthibeault@outlook.com>
2024-08-03 12:32:35 -0400

    place io code of the serial port to a separate io file

--- kernel/include/serial.h
+++ kernel/include/serial.h
@@ -4,8 +4,7 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include <stdarg.h>
-#include <stdint.h>
-
+#include <io.h>
 
 #ifdef __x86_64
 #define PORT 0x3f8          // COM1
@@ -15,23 +14,4 @@
 int init_serial();
 void write_serial(char a);
 void write_serial_str(const char *str);
-
-static inline void 
-outb(uint16_t port, uint8_t val) {
-    __asm__ volatile ( "outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
-    /* There's an outb %al, $imm8 encoding, for compile-time constant port numbers that fit in 8b. (N constraint).
-     * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
-     * The  outb  %al, %dx  encoding is the only option for all other cases.
-     * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
-}
-
-static inline uint8_t 
-inb(uint16_t port) {
-    uint8_t ret;
-    __asm__ volatile ( "inb %w1, %b0"
-                   : "=a"(ret)
-                   : "Nd"(port)
-                   : "memory");
-    return ret;
-}
 
