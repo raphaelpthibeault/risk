dc76051372ce4db2980773f540012e7b09d4d7a7
rpthibeault <rpthibeault@outlook.com>
2024-08-03 21:29:23 -0400

    throw it away and make it better

--- kernel/src/lib/log.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdarg.h>
-#include <lib/std/printf.h>
-#include <io/serial/serial.h>
-
-void log_(const char *, ...);
--- kernel/src/macros.h
+++
@@ -1,6 +0,0 @@
-#pragma once 
-
-#define PACKED              __attribute__((packed))
-/* round up n to the nearest multiple */
-#define ROUND_UP(n, mult)   ((n) % (mult) == 0 ? (n) : (n) - (n) % (mult) + (mult))
-
